5f904ba55d043b11d9da76753f7ac1bd00d76580
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-09-23 23:21:20 +0530

    [Gaurav]ADDED: Iterate_till_reach_win_position

--- SnakeLadderUc1.java
+++ SnakeLadderUc1.java
@@ -2,14 +2,14 @@ public class SnakeLadderUc1
 {
 	public static void main(String[] args)
 	{
-		int START_POS = 0;
+		int START_POS= 0;
 		int NO_PLAY = 1;
 		int SNAKE = 2;
 		int LADDER = 3;
 		int win_position = 100;
 		int position = START_POS;
 		
-		while(position != win_position)
+		while(position!= win_position)
 		{
 			int die_Number = (int)Math.floor(Math.random()*6 + 1);
 			int options = (int)Math.floor(Math.random()*3 + 1);
@@ -20,15 +20,19 @@ public class SnakeLadderUc1
 			}
 			else if(options == SNAKE)
 			{
-				position = position - die_Number;
-				if(position<0)
+				position= position- die_Number;
+				if(position < 0)
 				{
-					position = 0;
+					position= 0;
 				}
 			}
 			else if(options == LADDER)
 			{
-				position = position + die_Number;	
+				position= position + die_Number;	
+				if(position > win_position)
+				{
+					position= position- die_Number;
+				}
 			}
 		}		
 	}

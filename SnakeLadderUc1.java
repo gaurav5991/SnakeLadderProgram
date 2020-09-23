7df6fff346b53e9b45ac3750208f40202d9d8a48
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-09-23 23:37:54 +0530

    [Gaurav]ADDED: Game with 2 Players

--- SnakeLadderUc1.java
+++ SnakeLadderUc1.java
@@ -1,45 +1,102 @@
 public class SnakeLadderUc1
 {
-	public static void main(String[] args)
-	{
-		int START_POS = 0;
-		int NO_PLAY = 1;
-		int SNAKE = 2;
-		int LADDER = 3;
-		int win_position = 100;
-		int position = START_POS;
+	 public static void main(String[] args) 
+	 {
+		 int START_POS = 0;
+		 
+		 int NO_PLAY  = 1;
+		 
+		 int SNAKE = 2;
+		 
+		 int LADDER = 3;
+		 
+		 int rollCount1 = 0;
+		 int rollCount2 = 0;
 
-		int roll_count = 0;
-		
-		while(position != win_position)
-		{
-			int die_Number = (int)Math.floor(Math.random()*6 + 1);
-			int options = (int)Math.floor(Math.random()*3 + 1);
-
-			if(options == NO_PLAY)
-			{
-				position += 0;
-			}
-			else if(options == SNAKE)
-			{
-				position  = position - die_Number;
-				if(position < 0)
-				{
-					position = 0;
-				}
-			}
-			else if(options == LADDER)
-			{
-				position = position + die_Number;	
-				if(position > win_position)
-				{
-					position = position - die_Number;
-				}
-			}
-			roll_count++;
-			System.out.println("Position of Player: " +  position);
-		}		
-		
-		System.out.println("Number of Timed Die rolled: " + roll_count);
-	}
+		 int win_position = 10;
+	
+		 int player1_position = START_POS; 
+		 int player2_position = START_POS; 
+		 
+		 int PlayerTurn;
+		  
+		 int Player1 = 1;
+		 int Player2 = 2;
+		 		 		 
+		 while(player1_position != win_position && player2_position != win_position)
+		 {
+			 PlayerTurn = (int)Math.floor(Math.random()*2 + 1);
+			 if(PlayerTurn == Player1)
+			 {
+				 int die_number = (int)Math.floor(Math.random()*6 + 1);
+				 int options = (int)Math.floor(Math.random()*3 + 1);
+				 
+				 if(options == NO_PLAY)
+				 {
+					 player1_position = player1_position + 0;
+				 }
+				 else if(options == SNAKE)
+				 {
+					 player1_position = player1_position - die_number;
+					 if(player1_position<0)
+					 {
+						 player1_position = 0;
+					 }
+				 }
+				 else if(options == LADDER)
+				 {
+					 while(options == LADDER)
+					 {
+						 die_number = (int)Math.floor(Math.random()*6 + 1);
+						 options = (int)Math.floor(Math.random()*3 + 1);
+						 
+						 player1_position = player1_position + die_number;	 
+						 if(player1_position > win_position)
+						 {
+							 player1_position = player1_position - die_number;
+						 }
+					 }
+				 }
+				 rollCount1++;
+			 }
+			 else if(PlayerTurn == Player2)
+			 {
+				 int die_number = (int)Math.floor(Math.random()*6 + 1);
+				 int options = (int)Math.floor(Math.random()*3 + 1);
+				 
+				 if(options == NO_PLAY)
+				 {
+					 player2_position = player2_position + 0;
+				 }
+				 else if(options == SNAKE)
+				 {
+					 player2_position = player2_position - die_number;
+					 if(player2_position<0)
+					 {
+						 player2_position = 0;
+					 }
+				 }
+				 else if(options == LADDER)
+				 {
+					 while(options == LADDER)
+					 {
+						 die_number = (int)Math.floor(Math.random()*6 + 1);
+						 options = (int)Math.floor(Math.random()*3 + 1);
+						 
+						 player2_position = player2_position + die_number;	 
+						 if(player2_position >win_position)
+						 {
+							 player2_position = player2_position - die_number;
+						 }
+					 }
+				 }
+				 rollCount2++;			 
+		 }
+		 
+	 }
+		 if(rollCount1 > rollCount2)
+			System.out.println("Player 1 Won");
+		 else
+			System.out.println("Player 2 Won");
+}
 }
